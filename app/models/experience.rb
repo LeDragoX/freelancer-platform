610f5e117c4378de37e06e24f20238769d70e822
Plínio Larrubia <plinio2xd@gmail.com>
2021-10-19 16:59:48 -0300

    Fix custom validation breaking with 2 nil Dates

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -10,13 +10,13 @@ class Experience < ApplicationRecord
   private
 
   def started_at_lower_than_ended_at
-    if !started_at.nil? && started_at > ended_at
+    if (!started_at.nil? && !ended_at.nil?) && started_at > ended_at
       errors.add(:started_at, "não pode ser maior do que #{I18n.t(:ended_at, scope: "activerecord.attributes.experience")}")
     end
   end
 
   def ended_at_greater_than_started_at
-    if !ended_at.nil? && ended_at < started_at
+    if (!ended_at.nil? && !started_at.nil?) && ended_at < started_at
       errors.add(:ended_at, "não pode ser menor do que #{I18n.t(:started_at, scope: "activerecord.attributes.experience")}")
     end
   end
