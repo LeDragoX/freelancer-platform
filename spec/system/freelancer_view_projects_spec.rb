17690ec7d8cb6477c25f54e1f2c61e3ff952a982
Plínio Larrubia <plinio2xd@gmail.com>
2021-10-19 23:37:15 -0300

    Change the quotes

--- spec/system/freelancer_view_projects_spec.rb
+++ spec/system/freelancer_view_projects_spec.rb
@@ -1,56 +1,56 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'Freelancer view projects' do
-  context 'on home page' do
-    it 'and saw 1 project' do
-      freelancer = Freelancer.create!({ email: 'freelancer@test.com', password: '123456' })
-      user = User.create!({ email: 'user@test.com', password: '123456' })
-      job_remoto = JobType.create!({ name: 'Remoto' })
-      project = Project.create!({ title: 'Sistema Web',
-                                  description: 'Site de Fast Food por encomenda',
-                                  wanted_skills: 'No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS',
+describe "Freelancer view projects" do
+  context "on home page" do
+    it "and saw 1 project" do
+      freelancer = Freelancer.create!({ email: "freelancer@test.com", password: "123456" })
+      user = User.create!({ email: "user@test.com", password: "123456" })
+      job_remoto = JobType.create!({ name: "Remoto" })
+      project = Project.create!({ title: "Sistema Web",
+                                  description: "Site de Fast Food por encomenda",
+                                  wanted_skills: "No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS",
                                   max_hour_rate: 300, deadline: 4.month.from_now,
                                   available: true, job_type: job_remoto, user: user })
 
       login_as freelancer, scope: :freelancer
       visit root_path
 
-      within 'nav' do
-        expect(page).to_not have_content 'Meus Projetos'
-        expect(page).to_not have_content 'Novo Projeto'
+      within "nav" do
+        expect(page).to_not have_content "Meus Projetos"
+        expect(page).to_not have_content "Novo Projeto"
       end
-      expect(page).to have_link 'Sistema Web', href: project_path(project)
-      expect(page).to_not have_content 'R$ 600,00'
+      expect(page).to have_link "Sistema Web", href: project_path(project)
+      expect(page).to_not have_content "R$ 600,00"
       expect(page).to_not have_content I18n.l(project.deadline) # Foi o melhor jeito consegui de testar Data
     end
 
-    it 'enters the project page' do
-      freelancer = Freelancer.create!({ email: 'freelancer@test.com', password: '123456' })
-      user = User.create!({ email: 'user@test.com', password: '123456' })
-      job_presential = JobType.create!({ name: 'Presencial' })
-      project = Project.create!({ title: 'Sistema Web',
-                                  description: 'Site de Fast Food por encomenda',
-                                  wanted_skills: 'No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS',
+    it "enters the project page" do
+      freelancer = Freelancer.create!({ email: "freelancer@test.com", password: "123456" })
+      user = User.create!({ email: "user@test.com", password: "123456" })
+      job_presential = JobType.create!({ name: "Presencial" })
+      project = Project.create!({ title: "Sistema Web",
+                                  description: "Site de Fast Food por encomenda",
+                                  wanted_skills: "No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS",
                                   max_hour_rate: 300, deadline: 4.month.from_now,
                                   available: true, job_type: job_presential, user: user })
 
       login_as freelancer, scope: :freelancer
       visit root_path
-      click_on 'Sistema Web'
+      click_on "Sistema Web"
 
-      expect(page).to have_content 'Sistema Web'
-      expect(page).to have_content 'Site de Fast Food por encomenda'
-      within 'dl' do
+      expect(page).to have_content "Sistema Web"
+      expect(page).to have_content "Site de Fast Food por encomenda"
+      within "dl" do
         expect(page).to have_content project.user.email
-        expect(page).to have_content 'No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS'
-        expect(page).to have_content 'R$ 300,00'
+        expect(page).to have_content "No Back: NodeJS, Prisma, PostgreSQL. No Front: VueJS, Sass, Tailwind CSS"
+        expect(page).to have_content "R$ 300,00"
         expect(page).to have_content I18n.l(project.deadline)
-        expect(page).to have_content 'Presencial'
+        expect(page).to have_content "Presencial"
         expect(page).to have_content /Sim/ # no lugar do true
-        expect(page).to have_content I18n.t(project.status, scope: 'activerecord.attributes.statuses')
+        expect(page).to have_content I18n.t(project.status, scope: "activerecord.attributes.statuses")
       end
-      expect(page).to_not have_link 'Editar Projeto'
-      expect(page).to_not have_link 'Deletar Projeto'
+      expect(page).to_not have_link "Editar Projeto"
+      expect(page).to_not have_link "Deletar Projeto"
     end
   end
 end
--- spec/system/visitor_login_on_home_spec.rb
+++ spec/system/visitor_login_on_home_spec.rb
@@ -1,59 +1,59 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'Visitor login on Home' do
-  context 'as a normal visitor (no auth)' do
-    it 'successfully' do
+describe "Visitor login on Home" do
+  context "as a normal visitor (no auth)" do
+    it "successfully" do
       visit root_path
 
-      within 'nav' do
-        expect(page).to_not have_content 'Logout'
-        expect(page).to_not have_content 'Meus Projetos'
-        expect(page).to_not have_content 'Novo Projeto'
+      within "nav" do
+        expect(page).to_not have_content "Logout"
+        expect(page).to_not have_content "Meus Projetos"
+        expect(page).to_not have_content "Novo Projeto"
       end
-      expect(page).to_not have_content 'Profissionais disponíveis'
-      expect(page).to_not have_content 'Projetos disponíveis'
+      expect(page).to_not have_content "Profissionais disponíveis"
+      expect(page).to_not have_content "Projetos disponíveis"
     end
   end
 
-  context 'as an user' do
-    it 'successfully' do
-      user = User.create!({ email: 'user@test.com', password: '123456' })
+  context "as an user" do
+    it "successfully" do
+      user = User.create!({ email: "user@test.com", password: "123456" })
 
       visit root_path
-      click_on 'Entrar como Usuário'
-      fill_in 'E-mail',	with: user.email
-      fill_in 'Senha',	with: user.password
-      click_on 'Entrar'
+      click_on "Entrar como Usuário"
+      fill_in "E-mail",	with: user.email
+      fill_in "Senha",	with: user.password
+      click_on "Entrar"
 
-      within 'nav' do
-        expect(page).to have_content 'user@test.com'
-        expect(page).to have_content 'Logout'
-        expect(page).to have_content 'Meus Projetos'
-        expect(page).to have_content 'Novo Projeto'
+      within "nav" do
+        expect(page).to have_content "user@test.com"
+        expect(page).to have_content "Logout"
+        expect(page).to have_content "Meus Projetos"
+        expect(page).to have_content "Novo Projeto"
       end
-      expect(page).to have_content 'Profissionais disponíveis'
-      expect(page).to_not have_content 'Projetos disponíveis'
+      expect(page).to have_content "Profissionais disponíveis"
+      expect(page).to_not have_content "Projetos disponíveis"
     end
   end
 
-  context 'as a freelancer' do
-    it 'successfully' do
-      freelancer = Freelancer.create!({ email: 'freelancer@test.com', password: '123456' })
+  context "as a freelancer" do
+    it "successfully" do
+      freelancer = Freelancer.create!({ email: "freelancer@test.com", password: "123456" })
 
       visit root_path
-      click_on 'Entrar como Freelancer'
-      fill_in 'E-mail',	with: freelancer.email
-      fill_in 'Senha',	with: freelancer.password
-      click_on 'Entrar'
+      click_on "Entrar como Freelancer"
+      fill_in "E-mail",	with: freelancer.email
+      fill_in "Senha",	with: freelancer.password
+      click_on "Entrar"
 
-      within 'nav' do
-        expect(page).to have_content 'freelancer@test.com'
-        expect(page).to have_content 'Logout'
-        expect(page).to_not have_content 'Meus Projetos'
-        expect(page).to_not have_content 'Novo Projeto'
+      within "nav" do
+        expect(page).to have_content "freelancer@test.com"
+        expect(page).to have_content "Logout"
+        expect(page).to_not have_content "Meus Projetos"
+        expect(page).to_not have_content "Novo Projeto"
       end
-      expect(page).to have_content 'Projetos disponíveis'
-      expect(page).to_not have_content 'Profissionais disponíveis'
+      expect(page).to have_content "Projetos disponíveis"
+      expect(page).to_not have_content "Profissionais disponíveis"
     end
   end
 end
