85085e58790941573c35a3b38a934c130d30b894
Plínio Larrubia <plinio2xd@gmail.com>
2021-10-21 16:59:50 -0300

    Removed projects table partial (only use _project) - initialized OccupationAreas controller - move validation inside home controller to the beginning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @projects = Project.all
-    @freelancers = Freelancer.all
-    @profiles = Profile.all
-
     if freelancer_signed_in? && current_freelancer.profile.nil?
       redirect_to new_profile_path
     end
+
+    @projects = Project.all
+    @freelancers = Freelancer.all
+    @profiles = Profile.all
   end
 end
---
+++ app/controllers/occupation_areas_controller.rb
@@ -0,0 +1,3 @@
+class OccupationAreas < ApplicationController
+  
+end
